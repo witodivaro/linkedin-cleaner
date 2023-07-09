1c6754c1922fab043b6a8c4108e225eb49260be5
Vitali Litvinovich <witodivaro@witodivaro.local>
2023-07-09 12:48:45 +0200

    specify day in args

--- src/addRecruiters.js
+++ src/addRecruiters.js
@@ -6,6 +6,12 @@ const reqHeaders = require("../req-headers.json");
 
 prompt.start();
 
+const [_, __, day] = process.argv;
+
+const START = 430;
+const COUNT = 30;
+const START_DAY = Number(day);
+
 const urls = {
   fetchSecondConnections: "/voyager/api/search/dash/clusters",
   addConnection: "/voyager/api/voyagerRelationshipsDashMemberRelationships",
@@ -19,8 +25,8 @@ const options = {
     q: "all",
     query:
       "(keywords:recruiter,flagshipSearchIntent:SEARCH_SRP,queryParameters:(network:List(S),resultType:List(PEOPLE)),includeFiltersInResponse:false)",
-    start: 400,
-    count: 50,
+    start: START + (START_DAY * COUNT),
+    count: COUNT,
   },
   addConnection: {},
 };
@@ -108,13 +114,10 @@ const autoAdd = async () => {
   const members = await fetchConnections();
 
   for (const [idx, member] of members.entries()) {
-    if (idx === 30) {
-      console.log(`Day limit is reached`);
-      return;
-    }
-
     console.log(
-      `[${idx}] Adding ${member.name}\n${member.position}\n${member.location}\n${member.past}\n`
+      `[${idx}] Adding ${member.name}\nPOSITION: ${member.position || 'No information'}\nLOCATION: ${
+        member.location || 'No information'
+      }\n${member.past}\n`
     );
     await addConnection(member.entityUrn, member.name.split(" ")[0]);
     console.log("\n\n");
